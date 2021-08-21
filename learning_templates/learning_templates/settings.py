5ea343df824a12a4a59cc7cc5a7e51250e2decdf
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-08-21 07:03:50 +0530

    more changes to allowed hosts

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=lp-_yi5-*5ucb6vh1ud$dabm+^=1polpe1nn(75r_x=4u4_p%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thelostsinofenvy.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.thelostsinofenvy.pythonanywhere.com']
 
 # Application definition
 
