95542afafe763be692cae11e709e74045fb4b3e3
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-08-25 07:29:04 +0530

    Class Based views with basic httpresponse

--- cbv/basic_app/views.py
+++ cbv/basic_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from basic_app import views
-
-
+#from basic_app import views
+from django.views.generic import View
+from django.http import HttpResponse
 # Create your views here
-def index(request):
-    return render(request, 'basic_app/index.html')
+#def index(request):
+#   return render(request, 'basic_app/index.html')
+
+
+class CBView(View):
+    def get(self, request):
+        return HttpResponse("Class Based View")
