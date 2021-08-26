1129fddd54ca75bcc38b5e138a88dda02de94c9e
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-08-26 11:44:27 +0530

    List and Detail views using cbv's

--- cbv/basic_app/views.py
+++ cbv/basic_app/views.py
@@ -13,9 +13,11 @@ class IndexView(TemplateView):
 
 
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
 
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
-    template_name = 'basic_app /school_detail.html'
+    template_name = 'basic_app/school_detail.html'
